09992eacb2dacaf61f39ba9e9098e0ac8be92e80
Adriel Buenavides dos Santos <asantos@dataweb.com.br>
2021-01-08 17:32:54 -0300

    Web/API: inserido funcionalidades de edição de usuários.

---
+++ Api/Data/DatabaseContext.cs
@@ -0,0 +1,43 @@
+﻿using API.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Provider> Providers { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().HasData(new List<User>()
+            {
+                new User(1, "testeUser", "teste@gmail.com", "teste123"),
+            });
+
+            modelBuilder.Entity<Product>().HasData(new List<Product>
+            {
+                new Product(1, "produtoTeste", 87.5, 1, 1)
+            });
+
+            modelBuilder.Entity<Provider>().HasData(new List<Provider>
+            {
+                new Provider(1, "providerTeste", "cpnjFake")
+            });
+
+            modelBuilder.Entity<Brand>().HasData(new List<Brand>
+            {
+                new Brand(1, "brandTeste")
+            });
+        }
+
+    }
+}
